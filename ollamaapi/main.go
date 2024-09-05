93cb983dc40cdbc85c9ea476253b30f44832a9d8
root <claecedric@gmail.com>
2024-09-05 09:19:46 +0000

    initial api using gin

--- ollamaapi/main.go
+++ ollamaapi/main.go
@@ -3,8 +3,17 @@ package main
 import (
   "fmt"
   "1nsomnes/ollamaapi/webcalls"
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run()
+
   fmt.Println(webcalls.CallGemma2b("Respond with one word: \"yes\""))
 }
